4cca93a2da4011af68cb60610e10572f53734259
wzhaofei <wangzf@wellnkiot.com>
2022-08-30 16:51:55 +0800

    chore: 使用magic-string 合并代码

--- demo/magic-string.js
+++ demo/magic-string.js
@@ -2,3 +2,16 @@ const MagicString = require('magic-string')
 
 const magicString = new MagicString('export const name = "rollup"')
 console.log(magicString.remove(0, 7).toString())
+
+let bundleString = new MagicString.Bundle()
+bundleString.addSource({
+    content: 'const name = "rollup"',
+    separator: '\n\n'
+})
+
+bundleString.addSource({
+    content: "console.log(name)",
+    separator: '\n'
+})
+
+console.log(bundleString.toString())
